dc4cb339049c0cbc3dea0a23398d2164fd5bc4aa
sabbe212 <saber_samadi@outlook.com>
2024-11-22 14:37:19 +0100

    .

--- src/functions/createUser.js
+++ src/functions/createUser.js
@@ -1,13 +1,39 @@
 const { app } = require('@azure/functions');
-
+const {User}  = require('../../models');
+const bcrypt = require('bcrypt');
 app.http('createUser', {
-    methods: ['GET', 'POST'],
+    methods: ['POST'],
     authLevel: 'anonymous',
     handler: async (request, context) => {
-        context.log(`Http function processed request for url "${request.url}"`);
-
-        const name = request.query.get('name') || await request.text() || 'world';
-
-        return { body: `Hello, ${name}!` };
+        const data = await request.json();
+        const {username, name, lastname, email, password_hash} = data;
+        if(!username || !name || !lastname || !email || !password_hash) {
+            return {
+                status: 400,
+                body: 'Missing required fields'
+            };
+            return;
+        }
+        const saltRounds = 10;
+        const passwordHash = await bcrypt.hash(password_hash, saltRounds);
+        const newUser = await User.create({
+            username,
+            name,
+            lastname,
+            email,
+            passwordHash: passwordHash
+        });
+        return {
+            status: 201,
+            body: {
+                id: newUser.id,
+                username: newUser.username,
+                name: newUser.name,
+                lastname: newUser.lastname,
+                email: newUser.email
+                }
+            };
+        }
     }
-});
+);
+
